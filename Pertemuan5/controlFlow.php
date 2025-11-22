8529109fae95be1b2645f0c6c3bb201d5e6004ed
Arief Arafad <ariefarafad@gmail.com>
2025-11-22 22:58:32 +0700

    Wave 3 - Control Flow

---
+++ Pertemuan5/controlFlow.php
@@ -0,0 +1,24 @@
+<?php
+    $teman = 255520211015;
+
+    switch ($teman) {
+        case 255520211015:
+            echo "Arief Arafad";
+            break;
+        case 24101152610018:
+            echo "Kamila Wardah";
+            break;
+        case 24101152610008:
+            echo "Asria Nur Zafirah";
+            break;
+        case 24572011021:
+            echo "Syarla Azira";
+            break;
+        case 245720111021:
+            echo "Marsya Dalilla Putri";
+            break;
+        default:
+            echo "Sendiri..";
+            break;
+    }
+?>
